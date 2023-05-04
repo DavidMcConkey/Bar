170df5f667003fd5142d52f29257e8a591069221
david <davidmcconkey@outlook.com>
2023-05-04 13:58:04 -0400

    implement model

---
+++ server/models.py
@@ -0,0 +1,34 @@
+"""SQLAlchemy models for BarMate"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Cocktails(db.Model):
+    """Type of cocktails along with it's descriptors"""
+
+    __tablename__ = "cocktails"
+    iba = db.Column(db.Boolean)
+    name = db.Column(db.String(50),unique=True)
+    colors = db.Column(db.String(30),nullable=False)
+    glass = db.Column(db.String,nullable=False)
+    category = db.Column(db.String,nullable=False)
+    ingredients = db.Column(db.String,nullable=False)
+    garnish = db.Column(db.String,nullable=False)
+    preparation = db.Column(db.String,nullable=False)
+
+class Ingredients (db.Model):
+    """Type of ingredients for cocktail
+       ABV stands for alcohol by volume"""
+
+    __tablename__ = "ingredients"
+    name = db.Column(db.String(50),unqiue = True,nullable=False)
+    abv = db.Column(db.Integer,nullable=False)
+    taste = db.Column(db.String(50))
+
+
+def connect_db(app):
+    """conncects this db to flask app"""
+    db.app = app
+    db.init_app(app)
+    db.create_all()
